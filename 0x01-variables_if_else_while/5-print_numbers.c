a58bda6d1c1c7c6bd30e6eec002542fadf838a3e
LeahKamau <kamauleah35@gmail.com>
2022-09-09 16:15:11 -0700

    numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 0 to 9.
+ * main - prints 0 to 9
  *
- * Return: Always 0 (Success)
+ * Return: 0 always success
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for(i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints between 9 and 10
+ *
+ * Return: sucess 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
