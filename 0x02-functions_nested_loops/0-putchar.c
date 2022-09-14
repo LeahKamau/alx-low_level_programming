13c5af1faeb6d28185107c2d3e4bda46032926ba
LeahKamau <kamauleah35@gmail.com>
2022-09-14 13:54:22 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,14 @@ include "main.h"
  */
 int main(void)
 {
-	char lnk[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(lnk[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
