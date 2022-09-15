ee78c12f9f9663019326fef2a117277e493a3747
LeahKamau <kamauleah35@gmail.com>
2022-09-15 11:09:57 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,7 +35,7 @@ void print_times_table(int n)
 				}
 				else
 				{
-					_putchar(((prod / 100) % 10) + '0');
+					_putchar((prod / 100) + '0');
 					_putchar((prod / 10) + '0');
 					_putchar((prod % 10) + '0');
 				}
