4552e58bb6963191aa93326688eafe00fe16abfc
LeahKamau <kamauleah35@gmail.com>
2022-09-15 06:58:47 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * and returns result
+ *
+ * @a: input number type int
+ * @b: input number type int
+ *
+ * Return: adds a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
