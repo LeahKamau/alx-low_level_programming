cb77a288e33274a50ca7b2d2ef705f7cc06466e1
LeahKamau <kamauleah35@gmail.com>
2022-09-15 12:39:34 -0700

    sum of multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints natural numbers below 1024 
+ * main - prints natural numbers below 1024
  * that are multiples of 3 or 5
  *
  * Return: always 0
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int a, sumb = 0;
+
 	for (a = 1; a < 1024; a++)
 	{
 		if ((a % 3) == 0 || (a % 5) == 0)
