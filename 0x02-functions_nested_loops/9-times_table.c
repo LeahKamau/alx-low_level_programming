19dbd13b75efd2f4c3bf9745d22e2e5e3d99e75c
LeahKamau <kamauleah35@gmail.com>
2022-09-15 06:22:10 -0700

    prints timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (prod <= 9)
+			{
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
