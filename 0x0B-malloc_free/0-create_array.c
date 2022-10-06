c390914b01aed179858ae2464b3a1f6ce525c57c
LeahKamau <kamauleah35@gmail.com>
2022-10-06 06:24:19 -0700

    returns pointer to the array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * @c: stored char
  * Return: pointer of an array of chars
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	int index;
 	char *cr;
