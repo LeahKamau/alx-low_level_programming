61f11bcd79c181f3beb0fbba5e8fe3f918527086
LeahKamau <kamauleah35@gmail.com>
2022-10-06 06:20:49 -0700

    returns pointer to the array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates array of cahrs
+ * @size: size of array
+ * @c: stored char
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c);
+{
+	int index;
+	char *cr;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(sizeof(char) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		cr[index] = c;
+
+	return (array);
+}
+
+
