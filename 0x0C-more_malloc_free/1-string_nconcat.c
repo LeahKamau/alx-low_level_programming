321adfd80a732ce54d8b3793ee808467f91cbce8
LeahKamau <kamauleah35@gmail.com>
2022-10-06 20:23:33 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 	unsigned int len1, len2, i, lsout;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -43,6 +43,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			ptr[i] = s2[i - len1];
 
 	ptr[i] = '\0';
-	
+
 	return (ptr);
 }
