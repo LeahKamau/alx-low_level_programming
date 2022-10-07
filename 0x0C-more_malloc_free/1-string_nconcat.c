08eff423df1fb417851b67956a21c367527a895f
LeahKamau <kamauleah35@gmail.com>
2022-10-06 20:17:33 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdlib.h>
-/**
- * _strlen - calculate and return string length
- * @string: string
- * Return: string length
- */
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
 /**
  * string_nconcat - concatenates two strings
  * @s1: first string
@@ -27,30 +13,36 @@ int _strlen(char *string)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int num, len, i, j;
-
-	num = n;
-
+	unsigned int len1, len2, i, j;
+	
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (num < 0)
-		return (NULL);
-	if (num >= _strlen(s2))
-		num = _strlen(s2);
 
-	len = _strlen(s1) + num + 1;
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
 
-	ptr = malloc(sizeof(*ptr) * len);
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	if (n >= len2)
+		n = len2;
+
+	lsout = len1 + n;
+
+	ptr = malloc(lsout + 1);
+
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		ptr[i] = s1[i];
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
+	for (i = 0; i < lsout; i++)
+		if (i < len1)
+			ptr[i] = s1[i];
+		else
+			ptr[i] = s2[i - len1];
 
+	ptr[i] = '\0';
+	
 	return (ptr);
 }
