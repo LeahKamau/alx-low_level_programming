82556da2ee55c28b1663b58adfc96dfb11e6dec4
LeahKamau <38250907+LeahKamau@users.noreply.github.com>
2022-10-16 15:32:27 +0300

    Create 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Passes string name to func pointer f
- * @name: String to be passed to func pointer.
- */
+* print_name - function that prints a name
+* @name: name to be printed by function
+* @f: pointer to function to print name.
+* Return: Always(0) Success
+*/
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
