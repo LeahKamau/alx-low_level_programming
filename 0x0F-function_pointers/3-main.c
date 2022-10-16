329d74a2ee333cd936a2ae4e202a8e357c0d93e4
LeahKamau <kamauleah35@gmail.com>
2022-10-16 06:55:53 -0700

    C funtion pointer for performing simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "3-main.h"
+
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char argv[]){
+
+
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-main.h"
+
+/**
+ * op_add - Calc Addition
+ * @a: int arg
+ * @b: int arg
+ * Return: reuslt of addition
+ */
+int op_add(int a, int b){
+return (a + b);
+}
+
+/**
+ * op_sub - Calc Subtraction
+ * @a: int arg
+ * @b: int arg
+ * Return: result of subtraction
+ */
+int op_sub(int a, int b){
+return (a - b);
+}
+
+/**
+ * op_mul - Cacl Multiplication
+ * @a: int arg
+ * @b: int arg
+ * Return: result of multiplication
+ */
+int op_mul(int a, int b){
+return (a * b);
+}
+
+/**
+ * op_div - Calc Division
+ * @a: int arg
+ * @b: int arg
+ * Return: result of division
+ */
+int op_div(int a, int b){
+return (a / b);
+}
+
+/**
+ * op_mod - Calc modula
+ * @a: int argument 
+ * @b: int argument
+ * Return: results of modula
+ */
+int op_mod(int a, int b){
+return (a % b);
+}
