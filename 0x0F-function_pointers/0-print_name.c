5edf861c4e576c58652d34b76ef78374df3c95f9
LeahKamau <38250907+LeahKamau@users.noreply.github.com>
2022-10-16 14:38:30 +0300

    Trialing whitespaces removal

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,11 @@
 * @f: pointer to function to print name.
 * Return: Always(0) Success
 */
-void print_name(char *name, void (*f)(char *)){
-    if(name == NULL){
-       exit(EXIT_FAILURE);
-    }
-    f(name);
+void print_name(char *name, void (*f)(char *))
+{
+if(name == NULL || f == NULL) /* This checks whether a name is provided or the pointer f is receivinga func*/
+{
+return;
+}
+f(name);
 }
