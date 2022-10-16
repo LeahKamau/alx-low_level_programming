7c3badf8611822cc635a9f00412b8924805b56f4
LeahKamau <kamauleah35@gmail.com>
2022-10-16 04:56:03 -0700

    C function pointers for executing func with parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointer.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array
+ * @array: existing array.
+ * @size: size of array.
+ * @action: pointer to the function to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (array == NULL || action == NUL)
+return;
+}
