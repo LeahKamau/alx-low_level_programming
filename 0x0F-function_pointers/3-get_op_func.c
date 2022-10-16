19677b95c1419abad88d540d21c67194f9b45877
LeahKamau <38250907+LeahKamau@users.noreply.github.com>
2022-10-16 17:24:54 +0300

    Addition of allowed variables in the function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,25 @@
 #include "3-main.h"
 
 /**
- *
- *
- *
- *
+ * get_op_func - function pointer that selects correct function to perform.
+ * @s: operator passed as argument
+ * 
  */
 int (*get_op_func(char *s))(int, int)
 {
+    /* Struct ops accepts 1. operand 
+                          2. Pointer func
+    */
+        op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
 
+    while()
 
 }
