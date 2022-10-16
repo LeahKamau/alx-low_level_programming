387af5f03ba2ed729ae68d20a53364ae7f8fa680
LeahKamau <38250907+LeahKamau@users.noreply.github.com>
2022-10-16 17:41:20 +0300

    Close Semi-colon for print statements

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,18 +8,18 @@
  */
 int main(int argc, char *argv[]){
 if (argc != 4){
-printf("Error\n")
+printf("Error\n");
 exit(98);
 }
 num1 = atoi(argv[1]);
 operand = argv[2];
 num2 = atoi(argv[3]);
 if (operand != '/' || '*' || '+' || '-' || '%'){
-printf("Error\n")
+printf("Error\n");
 exit(99);
 } 
 if ((operand == '/' && num2 == 0) || operand == '%' && num2 == 0){
-printf("Error\n")
+printf("Error\n");
 exit(100);
 } 
 printf("%d\n",get_op_func(operand))(num1, num2))
