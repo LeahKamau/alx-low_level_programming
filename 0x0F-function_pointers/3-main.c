9fe4de755d03ab575e01208dceea29dd3f039f3b
LeahKamau <38250907+LeahKamau@users.noreply.github.com>
2022-10-16 17:11:02 +0300

    Funtionality of the main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3-main.h"
+#include "3-calc.h"
 
 /**
  *
@@ -6,7 +6,20 @@
  *
  */
 int main(int argc, char argv[]){
-
-
-
+if (argc != 4){
+printf("Error\n")
+exit(98);
+}
+num1 = atoi(argv[1]);
+operand = argv[2];
+num2 = atoi(argv[3]);
+if (operand != '/' || '*' || '+' || '-' || '%'){
+printf("Error\n")
+exit(99);
+} 
+if ((operand == '/' && num2 == 0) || operand == '%' && num2 == 0){
+printf("Error\n")
+exit(100);
+} 
+get_op_func(operand))(num1, num2)
 }
