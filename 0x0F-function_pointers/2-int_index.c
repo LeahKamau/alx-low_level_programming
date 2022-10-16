93ea2030cd738a98d1c9168591ed200cb16c5771
LeahKamau <kamauleah35@gmail.com>
2022-10-16 05:51:57 -0700

    C function pointers for Search for integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches for an integer
+ * @array: Array to search for int.
+ * @size: size of array
+ */
+int int_index(int *array, int size, int (*cmp)(int)) /*Int param in cmp is what is being searched*/
+{
+int i; /*Will be used in for loop to iterate array*/
+if (array == NULL || cmp == NULL)
+return;
+for (i = 0; i < size; i++) /*array[size] - i to iterate to max-size of array (size)*/
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
