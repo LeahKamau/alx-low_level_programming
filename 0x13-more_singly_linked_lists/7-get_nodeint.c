8f25dc8e018b00de9363f6ad0fad1fe7e8e00799
LeahKamau <kamauleah35@gmail.com>
2022-10-28 00:56:08 -0700

    get nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,15 +10,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	size_t i;
 
-	while (head != NULL)
+	for (i = 0; (i < index) && (head != NULL); i++)
 	{
-		if (i == index)
-			return (head->next);
-		i++;
 		head = head->next;
 	}
 
 	if (i < index)
 		return (NULL);
+
+	return (head);
+}
