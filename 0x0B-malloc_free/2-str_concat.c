6b47a7e84cadac7cc74f4a594153b30c3369041a
LeahKamau <kamauleah35@gmail.com>
2023-03-15 10:26:52 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,48 +2,44 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: pointer to an array of chars
+ * str_concat - concatates s1 and s2 to create new string
+ * @s1: string
+ * @s2: string
+ * Return: pointer to a string
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *strout;
-	unsigned int len1, len2, index1, index2, size;
+	char *s3;
+	int strlen1, strlen2, strlen3, index1, index2;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	strlen1 = 0;
 
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
+	while (s1[strlen1] != '\0')
+		strlen1++;
 
-	size = len1 + len2;
+	strlen2 = 0;
 
-	strout = malloc((sizeof(char) * size) + 1);
+	while (s2[strlen2] != '\0')
+		strlen2++;
 
-	if (strout == NULL)
+	strlen3 = strlen1 + strlen2;
+
+	s3 = (char *)malloc(sizeof(char) * (strlen3 + 1));
+
+	if (s3 == NULL)
 		return (NULL);
 
-	index1 = 0;
-	while (index1 < len1)
-	{
-		strout[index1] = s1[index1];
-		index1++;
-	}
+	for (index1 = 0; index1 < strlen1; index1++)
+		s3[index1] = s1[index1];
+
 	index2 = 0;
-	while (index1 <= size)
+
+	while (index1 <= strlen3)
 	{
-		strout[index1] = s2[index2];
-		index1++;
+		s3[index1] = s2[index2];
 		index2++;
+		index1++;
 	}
-	return (strout);
+
+	return (s3);
 }
