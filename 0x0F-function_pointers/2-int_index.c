93ccf380482da698e8d97b75845ec62b0b8177f9
LeahKamau <kamauleah35@gmail.com>
2023-03-23 03:02:21 -0700

    code fail

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,15 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
-		else
-			i++;
+		i++;
 	}
 
 	return (-1);
