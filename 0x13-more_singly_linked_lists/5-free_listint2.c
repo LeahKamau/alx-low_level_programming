9353803ddbc1f06f17403512714664468c9c5f45
LeahKamau <kamauleah35@gmail.com>
2023-05-02 15:18:49 -0700

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees linked list & sets
+ * head to NULL
+ * @head: pointer to  address of listint_t
+ *
+ * Return: no return
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while ((tmp = *head) != NULL)
+	{
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
