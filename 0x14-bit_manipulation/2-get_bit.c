9bd5e84768428c8f24272af4aba4d350da38cabd
LeahKamau <38250907+LeahKamau@users.noreply.github.com>
2023-05-05 04:28:48 +0300

    check fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,8 +13,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (n ==0 && index < 64)
-		return (0);
+	if (n ==0 && index > 63)
+		return (-1);
 
 	for (i = 0; i < 64; i++)
 	{
