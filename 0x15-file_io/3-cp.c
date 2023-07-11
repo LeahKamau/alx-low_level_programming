afb32963284c12d3385b747a50dc8b26c91cd028
LeahKamau <kamauleah35@gmail.com>
2023-07-10 18:51:57 -0700

    3. cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - copies file content to another
+ * @ac: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 (success)
+ */
+int main(int ac, char *argv[])
+{
+	int fptr_frm, fptr_to;
+	ssize_t rbytes;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fptr_frm = open(argv[1], O_RDONLY);
+
+	if (fptr_frm == -1)
+	{
+		close(fptr_frm);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fptr_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fptr_to == -1)
+	{
+		close(fptr_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((rbytes = read(fptr_frm, buffer, sizeof(buffer))) > 0)
+		write(fptr_to, buffer, rbytes);
+
+	if (close(fptr_frm) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fptr_frm);
+		exit(100);
+	}
+
+	if (close(fptr_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fptr_to);
+		exit(100);
+	}
+
+	return (0);
+}
