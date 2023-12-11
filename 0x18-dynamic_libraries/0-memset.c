97547a5dedae37761db7e55ef5d905c80b76d8bf
LeahKamau <kamauleah35@gmail.com>
2023-12-12 00:20:59 +0300

    dynamic libs

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - copies memory area,
+ * @s: destination memory area.
+ * @b: source memory area
+ * @n: bytes filled
+ * Return: the pointer to dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
