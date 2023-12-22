ce3c834b1d2c5b6c8a74c471379782b3dc20fae2
LeahKamau <kamauleah35@gmail.com>
2023-12-22 12:43:52 -0800

    3. >>> ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: hash table to update
+ * @key: key passed
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new = NULL, *tmp;
+
+	if (!ht || !key || !*key)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
+
